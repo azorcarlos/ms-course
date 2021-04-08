807ab5143058a85a6aee80a01afc0064c2e00433
Azor Carlos <azorcarlos@gmail.com>
2021-04-08 11:37:20 -0300

    gr-payroll implementação mock

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -1,2 +1,47 @@
-package com.devsuperior.hrpayroll.entities;public class Payment {
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+    private String nome;
+    private Double dailyIncome;
+    private int days;
+
+    public Payment() {
+
+    }
+
+    public Payment(String nome, Double dailyIncome, int days) {
+        this.nome = nome;
+        this.dailyIncome = dailyIncome;
+        this.days = days;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public double getTotal(){
+        return days * dailyIncome;
+    }
 }
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/servies/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/servies/PaymentService.java
@@ -1,2 +1,14 @@
-package com.devsuperior.hrpayroll.servies;public class PaymentService {
+package com.devsuperior.hrpayroll.servies;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(Long workerId, int days){
+        return new Payment("Bab",200.00, days);
+
+    }
+
 }
