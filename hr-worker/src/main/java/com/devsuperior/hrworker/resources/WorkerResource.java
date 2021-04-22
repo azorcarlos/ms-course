f277efb064a7e271dcc1d0d7f39e1a5de996f283
azorcarlos <azorcarlos@gmail.com>
2021-04-22 08:30:23 -0300

    Implementação LoadBalance

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -3,7 +3,10 @@ package com.devsuperior.hrworker.resources;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +20,13 @@ import com.devsuperior.hrworker.repository.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
-	private WorkerRepository repository;
-
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Autowired
+	private Environment env;
+	
+	private WorkerRepository repository;
+		
 	@Autowired
 	public WorkerResource(WorkerRepository repository) {
 		this.repository = repository;
@@ -33,6 +40,9 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("Porta = " + env.getProperty("local.server.port"));
+		
 		Optional<Worker> obj = repository.findById(id);
 
 		if(obj.isPresent()){
